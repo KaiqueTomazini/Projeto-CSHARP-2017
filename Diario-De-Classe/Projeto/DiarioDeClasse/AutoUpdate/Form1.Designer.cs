c75eca1374bf4571d8215842ba78d2d457c82e74
Kaique Tomazini Alves Pereira <46302870+KaiqueTomazini@users.noreply.github.com>
2019-11-22 00:09:56 -0300

    Add files via upload

---
+++ Diario-De-Classe/Projeto/DiarioDeClasse/AutoUpdate/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AutoUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVelocidade = new System.Windows.Forms.Label();
+            this.lblPorcentagem = new System.Windows.Forms.Label();
+            this.labelBaixado = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblVelocidade
+            // 
+            this.lblVelocidade.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVelocidade.Location = new System.Drawing.Point(289, 9);
+            this.lblVelocidade.Name = "lblVelocidade";
+            this.lblVelocidade.Size = new System.Drawing.Size(165, 31);
+            this.lblVelocidade.TabIndex = 0;
+            this.lblVelocidade.Text = "label1";
+            this.lblVelocidade.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPorcentagem
+            // 
+            this.lblPorcentagem.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorcentagem.Location = new System.Drawing.Point(139, 50);
+            this.lblPorcentagem.Name = "lblPorcentagem";
+            this.lblPorcentagem.Size = new System.Drawing.Size(315, 31);
+            this.lblPorcentagem.TabIndex = 1;
+            this.lblPorcentagem.Text = "label2";
+            this.lblPorcentagem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelBaixado
+            // 
+            this.labelBaixado.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBaixado.Location = new System.Drawing.Point(86, 91);
+            this.labelBaixado.Name = "labelBaixado";
+            this.labelBaixado.Size = new System.Drawing.Size(368, 31);
+            this.labelBaixado.TabIndex = 2;
+            this.labelBaixado.Text = "label3";
+            this.labelBaixado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelBaixado.Click += new System.EventHandler(this.labelDownloaded_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 132);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(442, 23);
+            this.progressBar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Velocidade do Download:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Percorrido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poor Richard", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 167);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.labelBaixado);
+            this.Controls.Add(this.lblPorcentagem);
+            this.Controls.Add(this.lblVelocidade);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Atualizador Diário de Classe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVelocidade;
+        private System.Windows.Forms.Label lblPorcentagem;
+        private System.Windows.Forms.Label labelBaixado;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+    }
+}
+
---
+++ Diario-De-Classe/Projeto/DiarioDeClasse/AutoUpdate/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Ionic.Zip;
+
+namespace AutoUpdate
+{
+    public partial class Form1 : Form
+    {
+        WebClient webClient;
+        Stopwatch sw = new Stopwatch();
+        
+
+        public void DownloadFile(string urlAddress)
+        {
+            using (webClient = new WebClient())
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                Uri URL = urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("http://" + urlAddress);
+                sw.Start();
+
+                try
+                {
+                    webClient.DownloadFileAsync(URL, "DiarioDeClasse.exe");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            lblVelocidade.Text = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+            progressBar.Value = e.ProgressPercentage;
+            lblPorcentagem.Text = e.ProgressPercentage.ToString() + "%";
+            labelBaixado.Text = string.Format("{0} MB's / {1} MB's",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+            if (progressBar.Value == 100)
+            {
+                Process.Start(@"DiarioDeClasse.exe");
+                Environment.Exit(1);
+            }
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                string error = e.Error.ToString();
+                MessageBox.Show(error);
+                return;
+            }
+            sw.Reset();
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Download has been canceled.");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(174)))), ((int)(((byte)(206)))));
+            labelBaixado.Text = "";
+            lblPorcentagem.Text = "";
+            lblVelocidade.Text = "";
+            DownloadFile(@"http://www.dropbox.com/s/peozphwq247yvvl/DiarioDeClasse.exe?dl=1");
+        }
+
+        private void labelDownloaded_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
